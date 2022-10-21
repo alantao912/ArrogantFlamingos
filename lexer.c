b96977f6de02362c13a4fe64ed45a2053678219b
Alan Tao <70001632+alantao912@users.noreply.github.com>
2022-10-21 04:49:14 +0000

    Refactored lexing function

--- lexer.c
+++ lexer.c
@@ -9,24 +9,25 @@
   Phone#: (470) 918 - 7623
 */
 
+#define NO_MATCH -1
 #define BLOCK_COMMENT_START 0
 #define LINE_COMMENT_START 1
 
-const int num_symbols = 5;
-const char *reserved_symbols[] = {"/*", "//", "interface", "while", "continue"};
+const int num_symbols = 6;
+const char *reserved_symbols[] = {"/*", "//", "interface", "while", "continue", "++"};
 
-const int num_int_chars = 6;
-const char interrupt_chars[] = {'+', '-', '*', '/', '(', ')'};
+const int num_int_chars = 9;
+const char interrupt_chars[] = {'+', '-', '*', '/', '(', ')', '"', '.', '='};
 
-enum mode { REGULAR, IDENTIFIER, STRING_LITERAL, NUM_LITERAL, BLOCK_COMMENT, LINE_COMMENT };
+enum mode { REGULAR, STRING_LITERAL, NUM_LITERAL, BLOCK_COMMENT, LINE_COMMENT };
 
 Toklist *init_toklist() {
-  Toklist *toklist = (Toklist *)malloc(sizeof(Toklist));
+  Toklist *toklist = (Toklist *) malloc(sizeof(Toklist));
   if (!toklist) {
     printf("OOM during lexing, exiting!\n");
     exit(-1);
   }
-  toklist->toklist = (Tok *)malloc(8 * sizeof(Tok));
+  toklist->toklist = (Tok *) malloc(8 * sizeof(Tok));
   if (!toklist->toklist) {
     printf("OOM during lexing, exiting!\n");
     exit(-1);
@@ -37,12 +38,12 @@ Toklist *init_toklist() {
 }
 
 StrBuff *init_strbuff() {
-  StrBuff *buff = (StrBuff *)malloc(sizeof(StrBuff));
+  StrBuff *buff = (StrBuff *) malloc(sizeof(StrBuff));
   if (!buff) {
     printf("OOM during lexing, exiting!\n");
     exit(-1);
   }
-  buff->buff = (char *)malloc(8);
+  buff->buff = (char *) malloc(8);
   if (!buff->buff) {
     printf("OOM during lexing, exiting!\n");
     exit(-1);
@@ -53,7 +54,7 @@ StrBuff *init_strbuff() {
 }
 
 static inline bool resize_strbuff(StrBuff *buff) {
-  char *new_buff = (char *)malloc(2 * buff->capacity);
+  char *new_buff = (char *) malloc(2 * buff->capacity);
   if (!new_buff) {
     return false;
   }
@@ -65,7 +66,7 @@ static inline bool resize_strbuff(StrBuff *buff) {
 }
 
 static inline bool resize_toklist(Toklist *toklist) {
-  Tok *new_toklist = (Tok *)malloc(2 * toklist->capacity * sizeof(Tok));
+  Tok *new_toklist = (Tok *) malloc(2 * toklist->capacity * sizeof(Tok));
   if (!new_toklist) {
     return false;
   }
@@ -110,7 +111,7 @@ inline void push_toklist(Toklist *toklist, Tok t) {
 Toklist *lex(FILE *src) {
   Toklist *toklist = init_toklist();
   const int chunk_size = 1024;
-  char *buff = (char *)malloc(chunk_size + 2);
+  char *buff = (char *) malloc(chunk_size + 2);
   if (!buff) {
     printf("OOM during lexing, exiting!\n");
     exit(-1);
@@ -130,73 +131,92 @@ Toklist *lex(FILE *src) {
       char c = buff[i];
       switch (state) {
       case REGULAR:
-        push_strbuff(char_stack, c);
-        if (!should_continue(char_stack)) {
-          pop_strbuff(char_stack);
+        if (is_interrupt_char(c) && char_stack->size) {
           int match = matches_reserved_symbol(char_stack);
-          if (match == BLOCK_COMMENT_START) {
-            state = BLOCK_COMMENT;
-            clear_strbuff(char_stack);
-          } else if (match == LINE_COMMENT_START) {
-            state = LINE_COMMENT;
-            clear_strbuff(char_stack);
-          } else if (match != -1) {
-            /* Matches some charsequence*/
-            Tok t = {(unsigned char)match, NULL};
+          if (match == NO_MATCH) {
+            /* Identifier has been found */
+            char *identifier = strndup(char_stack->buff, char_stack->size);
+            if (!identifier) {
+              printf("OOM during lexing, exiting!");
+              exit(-1);
+            }
+            Tok t = {0, identifier};
             push_toklist(toklist, t);
             clear_strbuff(char_stack);
-            if (c != ' ') {
-              push_strbuff(char_stack, c);
-            }
-          } else if (c == ' ') {
-            if (char_stack->size) {
-              state = IDENTIFIER;
-              continue;
-            }
-          } else if (!is_interrupt_char(c)) {
-            /* char_stack holds an identifier */
-            push_strbuff(char_stack, c);
-            state = IDENTIFIER;
+            if (c != ' ') push_strbuff(char_stack, c);
+          } else if (match == BLOCK_COMMENT_START) {
+            /* Encountered the start of a block comment */
+            clear_strbuff(char_stack);
+            state = BLOCK_COMMENT;
+          } else if (match == LINE_COMMENT_START) {
+            /* Encountered double slash identifying line comment */
+            clear_strbuff(char_stack);
+            state = LINE_COMMENT;
           } else {
-            set_strbuff(char_stack, c);
+            /* Encountered a reserved word or symbol */
+            Tok t = {match, NULL};
+            push_toklist(toklist, t);
+            clear_strbuff(char_stack);
+            if (c != ' ') push_strbuff(char_stack, c);
           }
+          
           if (c == '"') {
+            /* Encountered an open quote. Start of string literal. */
             state = STRING_LITERAL;
             clear_strbuff(char_stack);
-          } else if (is_numeric(c)) {
-            state = NUM_LITERAL;
-            set_strbuff(char_stack, c);
           }
-        }
-        break;
-      case IDENTIFIER:
-        if (c == ' ' || is_interrupt_char(c)) {
-          char *identifier = strndup(char_stack->buff, char_stack->size);
-          if (!identifier) {
-            printf("OOM during lexing, exiting!\n");
-            exit(-1);
-          }
-          Tok t = {0, identifier};
-          push_toklist(toklist, t);
+        } else if (c == '"') {
           clear_strbuff(char_stack);
-          push_strbuff(char_stack, c);
-          state = REGULAR;
-        } else {
+          state = STRING_LITERAL;
+        } else if (is_numeric(c)) {
+          set_strbuff(char_stack, c);
+          state = NUM_LITERAL;
+        } else if (c != ' ') {
+          /* Pushing non space character to the buffer */
           push_strbuff(char_stack, c);
         }
         break;
       case STRING_LITERAL:
         if (flags == 2) {
-          if (c == 't') {
-            push_strbuff(char_stack, (char) 0x09);
-          } else if (c == 'n') {
-            push_strbuff(char_stack, (char) 0x0A);
-          } else if (c == 'r') {
-            push_strbuff(char_stack, (char) 0x0D);
-          } else if (c == '"') {
-            push_strbuff(char_stack, '"');
-          } else if (c == '\\') {
-            push_strbuff(char_stack, '\\');
+          switch(c) {
+            case 'a':
+            push_strbuff(char_stack, (char) (0x07));
+            break;
+            case 'b':
+            push_strbuff(char_stack, (char) (0x08));
+            break;
+            case 'e':
+            push_strbuff(char_stack, (char) (0x1B));
+            break; 
+            case 'f':
+            push_strbuff(char_stack, (char) (0x0C));
+            break;
+            case 'n':
+            push_strbuff(char_stack, (char) (0x0A));
+            break;
+            case 'r':
+            push_strbuff(char_stack, (char) (0x0D));
+            break;
+            case 't':
+            push_strbuff(char_stack, (char) (0x09));
+            break;
+            case 'v':
+            push_strbuff(char_stack, (char) (0x0B));
+            break;
+            case '\\':
+            push_strbuff(char_stack, (char) (0x5C));
+            break; 
+            case '\'':
+            push_strbuff(char_stack, (char) (0x27));
+            break;
+            case '"':
+            push_strbuff(char_stack, (char) (0x22));
+            break;
+            case '?':
+            push_strbuff(char_stack, (char) (0x3F));
+            break;
+            default:
+            push_strbuff(char_stack, c);
           }
           flags >>= 1;
         } else if (c == '\\') {
@@ -210,13 +230,33 @@ Toklist *lex(FILE *src) {
           Tok t = {1, literal};
           push_toklist(toklist, t);
           clear_strbuff(char_stack);
+          flags = 1;
           state = REGULAR;
         } else {
           push_strbuff(char_stack, c);
         }
         break;
       case NUM_LITERAL:
-
+        if (is_numeric(c)) {
+          push_strbuff(char_stack, c);
+        } else if (c == '.' && flags == 1) {
+          /* First time a period character is encountered */
+          push_strbuff(char_stack, c);
+          ++flags;
+        } else {
+          char *value = strndup(char_stack->buff, char_stack->size);
+          if (!value) {
+            printf("OOM during lexing, exiting!\n");
+            exit(-1);
+          }
+          Tok t = {2, value};
+          push_toklist(toklist, t);
+          clear_strbuff(char_stack);
+          if (c != ' ') push_strbuff(char_stack, c);
+          
+          flags = 1;
+          state = REGULAR;
+        }
         break;
       case BLOCK_COMMENT:
         if (flags == 1) {
@@ -225,9 +265,11 @@ Toklist *lex(FILE *src) {
           }
         } else if (flags == 2) {
           if (c == '/') {
+            flags = 1;
             state = REGULAR;
+          } else {
+            flags >>= 1;
           }
-          flags = 1;
         }
         break;
       case LINE_COMMENT:
@@ -263,7 +305,7 @@ int matches_reserved_symbol(const StrBuff *buff) {
   END:
     ++i;
   }
-  return -1;
+  return NO_MATCH;
 }
 
 bool should_continue(const StrBuff *buff) {
@@ -290,12 +332,22 @@ bool should_continue(const StrBuff *buff) {
 }
 
 bool is_interrupt_char(char c) {
-  for (int i = 0; i < num_int_chars; ++i) {
-    if (interrupt_chars[i] == c) {
-      return true;
+  static bool isInterruptMode = false;
+  if (isInterruptMode) {
+    return !(isInterruptMode = !(is_alpha(c) || is_numeric(c) || c == ' ' || c == '"'));
+  } else {
+    for (int i = 0; i < num_int_chars; ++i) {
+      if (interrupt_chars[i] == c) {
+        isInterruptMode = true;
+        return true;
+      }
     }
+    return c == ' ';
   }
-  return false;
+}
+
+inline bool is_alpha(char c) {
+  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 }
 
 inline bool is_numeric(char c) { return c >= '0' && c <= '9'; }
